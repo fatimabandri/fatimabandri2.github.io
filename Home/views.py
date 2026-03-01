83819f4a605301092bf809b4f7e83a1e3339c51a
Mehrinwagle <mehrinwagle26@gmail.com>
2026-03-01 14:41:57 +0530

    file added

--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from.models import Home
-# Create your views here.
+from .models import Home
+
 def home(request):
-    data=Home.objects.all()
-    return render(request,'index.html',{'data':data})
-    
+    data = Home.objects.all()
+    return render(request, 'index.html', {'data': data})
